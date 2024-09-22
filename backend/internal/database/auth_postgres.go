2d16a4fc82aef5db14c4edc9a26a788aeb5dc55a
maxbroker <camosval174@gmail.com>
2024-09-22 21:59:24 +0500

    fix config

--- backend/internal/database/auth_postgres.go
+++ backend/internal/database/auth_postgres.go
@@ -15,8 +15,8 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 }
 func (auth *AuthPostgres) CreateUser(user user.User) (int, error) {
 	var id int
-	query := fmt.Sprintf("INSERT INTO %s (email, password_hash) VALUES ($1, $2) RETURNING id", userTable)
-	row := auth.db.QueryRow(query, user.Email, user.Password)
+	query := fmt.Sprintf("INSERT INTO %s (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id", userTable)
+	row := auth.db.QueryRow(query, user.Name, user.Email, user.Password)
 	if err := row.Scan(&id); err != nil {
 		return 0, err
 	}
--- backend/internal/models/user/user.go
+++ backend/internal/models/user/user.go
@@ -2,6 +2,7 @@ package user
 
 type User struct {
 	Id       int    `json:"-"`
+	Name     string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
