93c30a0c5b19925c8f85d502b46698f2787cfd07
Aleksandr Tumanov <tumalexx02@gmail.com>
2024-09-25 02:40:33 +0500

    Styling :focus of some components

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import styles from "./Button.module.css";
 import cn from "classnames";
 import { IButtonProps } from './Button.props';
 
-export function Button({ children, size, accent = false, className, shape = 'default', outlined = false, ...props }: IButtonProps) {
+export function Button({ children, size = 'medium', accent = false, className, shape = 'default', outlined = false, ...props }: IButtonProps) {
   return (
     <button {...props} className={cn(className, styles["button"], styles[size], styles[shape], {
       [styles['accent']]: accent,
--- frontend/src/layouts/Auth/AuthLayout.tsx
+++ frontend/src/layouts/Auth/AuthLayout.tsx
@@ -3,13 +3,18 @@ import styles from './AuthLayout.module.css';
 import cn from 'classnames';
 import { Logo } from '../../components/Logo/Logo';
 import { ToggleThemeButton } from '../../components/ToggleThemeButton/ToggleThemeButton';
+import { MouseEvent } from 'react';
 
 function AuthLayout() {
+
+  const handleClick = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.currentTarget.blur();
+  };
 
   return (
     <div className={cn(styles['layout'])}>
       <header className={styles['header']}>
-        <Link to='/auth/login' className={styles['logo']}>
+        <Link to='/auth/login' className={styles['logo']} onClick={handleClick}>
           <Logo fill={'var(--text-color)'} />
         </Link>
 
